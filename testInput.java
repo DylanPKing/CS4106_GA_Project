ddd060e044ae7abb3f7d831cfbcf53c0b0e4767e
Bmalone100 <36305546+Bmalone100@users.noreply.github.com>
2019-11-05 12:59:48 +0000

    Max row/Max Col
    Got as far as retriving max values for matrix

--- testInput.java
+++ testInput.java
@@ -1,31 +1,53 @@
-public static void main String[] args
-{
-    File inputFile = new File("input.txt");
-    Scanner in = new Scanner(System.in);
-    int maxCol = 0;
-    int maxRow = 0;
-    int temp = 0;
-    String toParse = "";
-    int[][] matrix = new int[maxCol][maxRow];
-
-        while(inputFile.hasNextLine())
-        {
-            toParse = inputFile.readLine();
-            temp = Integer.parseInt(toParse.split(" "));
-                    for(int r = 0;r<1; r++)
-                    {
-                         for(int c = 0;c<1; c++)
-                        {
-                            matrix[r][c] = temp;
-                            System.out.print(matrix[r][c]);
-                            if(r%2 == 0)
-                            System.out.print("\n");
-                             if(matrix[r] < matrix[r+1])
-                                maxRow = matrix[r+1];
-                             if(matrix[c] < matrix[c+1])
-                                maxCol = matrix[c+1];
-                            System.out.print("Max Coloumn: " + maxCol + " " + "Max Row: " + maxRow);
-                        }
-                    }
+import java.util.*;
+import java.io.*;
+public class testInput{
+    public static void main(String[] args) throws IOException{
+        File inputFile = new File("input.txt");
+        Scanner in = new Scanner(inputFile);
+        int maxCol = 0;
+        int maxRow = 0;
+        int temp = 0;
+        ArrayList<ArrayList<Integer>> edgeTable = new ArrayList<ArrayList<Integer>>();
+        edgeTable.add(new ArrayList<Integer>());
+        edgeTable.add(new ArrayList<Integer>());
+        String[] split;
+        System.out.println(inputFile.exists());
+        /*
+        Split file contents into temp variable, then fill matrix.
+        */
+        while(in.hasNext()){
+            split = in.nextLine().split(" ");
+            for(int i = 0; i<split.length;i++){
+                temp = Integer.parseInt(split[i]);
+                if(i%2==0)
+                    edgeTable.get(0).add(temp);
+                else
+                    edgeTable.get(1).add(temp);
+            }
         }
+        for(int i=0;i<edgeTable.size();i++){
+            for(int j=0;j<edgeTable.get(i).size();j++)
+                System.out.print(edgeTable.get(i).get(j) + " ");
+            System.out.println();
+        }
+        /*
+        Assign Max Row value
+        */
+        for(int j =0; j<edgeTable.get(0).size() - 1;j++)
+            maxRow = Math.max(maxRow, Math.max(edgeTable.get(0).get(j), edgeTable.get(0).get(j+1)));
+        System.out.println("Max Row: " + maxRow);
+        /*     
+        Assign Max Coloumn value
+        */
+        for(int j =0; j<edgeTable.get(1).size() - 1;j++)
+            maxCol = Math.max(maxCol, Math.max(edgeTable.get(1).get(j), edgeTable.get(1).get(j+1)));
+        System.out.println("Max Column: "+maxCol);
+        /*
+        Create the adjacency Matrix
+        */
+        int[][] matrix = new int[maxCol][maxRow];
+
+
+
+    }
 }
